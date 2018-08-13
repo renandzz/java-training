9587a37429c3b23992e793b4988eef406b0b250f
renando <renan@tiket.com>
2018-08-13 18:56:17 +0700

    Commit no 8

--- src/com/company/Question7.java
+++ src/com/company/Question7.java
@@ -12,21 +12,52 @@ public class Question7 {
     int pattern = scanner.nextInt();
 
     int number = 1;
+    int revnumber = 6;
 
     switch (pattern){
       case 1:
         for (int i = 0; i < 6 ; i++) {
-          for (int j = 0; j < 6 ; j++) {
+          for (int j = 0; j < number ; j++) {
             System.out.print((j+1)+" ");
           }
+          number+=1;
           System.out.println();
         }
         break;
       case 2:
+        for (int i = 0; i < 6 ; i++) {
+          for (int j = 0; j < revnumber ; j++) {
+            System.out.print((j + 1) + " ");
+          }
+          revnumber -= 1;
+          System.out.println();
+        }
         break;
       case 3:
+        for (int i = 0; i < 6 ; i++) {
+          for (int j = 0; j < revnumber ; j++) {
+            System.out.print("  ");
+          }
+          for (int j = 0; j < number ; j++) {
+            System.out.print((j+1)+" ");
+          }
+          revnumber -= 1;
+          number+=1;
+          System.out.println();
+        }
         break;
       case 4:
+        for (int i = 0; i < 6 ; i++) {
+          for (int j = 0; j < number ; j++) {
+            System.out.print("  ");
+          }
+          for (int j = 0; j < revnumber ; j++) {
+            System.out.print((j+1)+" ");
+          }
+          revnumber -= 1;
+          number+=1;
+          System.out.println();
+        }
         break;
         default:break;
     }
